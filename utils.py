e7d8eed874caceee318817e5f409fe042ce4cb5a
Carola Segale <carolasegale@gmail.com>
2025-08-05 14:45:31 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,9 @@ def download_file(task_id, file_name):
 
     # Save the file
     path = os.path.join("content", file_name)
+    print(path)
     with open(path, "wb") as f:
         f.write(response.content)
-
+    
+    print(os.listdir("content/"))  # List files in the content directory to verify download
     print("Downloaded:", file_name)
